75df0aef6e114790debb3c8250d87f2ea13b4b74
ZalankiMate <zalanki.mate7@gmail.com>
2025-04-23 16:43:38 +0200

    Message and key validation
    Implemented two def statements to check for valid inputs.
    Added code_sum to optimize the if statement that checks the sum of message_code and key_code.

--- Feladat.py
+++ Feladat.py
@@ -9,6 +9,10 @@ def code_to_char(code):
     return chr(code + ord('a'))
 
 def encrypt(message, key):
+    if not key_check(key):
+        raise ValueError("The key is only allowed to contain lowercase letters of the english alphabet and space")
+    if not message_check(message):
+        raise ValueError("The message is only allowed to contain lowercase letters of the english alphabet and space")
     if len(key) < len(message):
         raise ValueError("The key can't be shorter than the message.")
 
@@ -17,14 +21,29 @@ def encrypt(message, key):
     for message_char, key_char in zip(message, key):
         message_code = char_to_code(message_char)
         key_code = char_to_code(key_char)
-        if ((message_code + key_code)>26):
-            encrypted_code = (message_code + key_code) % 27
+        code_sum = message_code + key_code
+        if (code_sum>26):
+            encrypted_code = code_sum % 27
         else:
-            encrypted_code = (message_code + key_code)
+            encrypted_code = code_sum
         encrypted_char = code_to_char(encrypted_code)
         encrypted.append(encrypted_char)
 
     return ''.join(encrypted)
+
+def message_check(message):
+    allowed_chars = set("abcdefghijklmnopqrstuvwxyz ")
+    for character in message:
+        if character not in allowed_chars:
+            return False
+    return True
+
+def key_check(key):
+    allowed_chars = set("abcdefghijklmnopqrstuvwxyz ")
+    for character in message:
+        if character not in allowed_chars:
+            return False
+    return True
 
 message = "helloworld"
 key = "abcdefgijkl"
