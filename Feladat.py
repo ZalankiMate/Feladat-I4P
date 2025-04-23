2c237828269ca6ce807d6acdbbb85a51697aff8d
ZalankiMate <zalanki.mate7@gmail.com>
2025-04-23 15:16:09 +0200

    Encryption
    Encrypts the message based on the key

--- Feladat.py
+++ Feladat.py
@@ -7,3 +7,18 @@ def code_to_char(code):
     if code == 26:
         return ' '
     return chr(code + ord('a'))
+
+def encrypt(message, key):
+    if len(key) < len(message):
+        raise ValueError("The key can't be shorter than the message.")
+
+    encrypted = []
+
+    for message_char, key_char in zip(message, key):
+        message_code = char_to_code(message_char)
+        key_code = char_to_code(key_char)
+        encrypted_code = (message_code + key_code) % 27
+        encrypted_char = code_to_char(encrypted_code)
+        encrypted.append(encrypted_char)
+
+    return ''.join(encrypted)
